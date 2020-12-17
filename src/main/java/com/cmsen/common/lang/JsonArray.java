4a73bc028df4185a66c0c3150e78dac6ebe62941
Jared.Yan <fuile@qq.com>
2020-12-17 09:58:50 +0800

    update 2.0.0

---
+++ src/main/java/com/cmsen/common/lang/JsonArray.java
@@ -0,0 +1,13 @@
+/**
+ * +---------------------------------------------------------
+ * | Author Jared.Yan<yanhuaiwen@163.com>
+ * +---------------------------------------------------------
+ * | Copyright (c) http://cmsen.com All rights reserved.
+ * +---------------------------------------------------------
+ */
+package com.cmsen.common.lang;
+
+import java.util.ArrayList;
+
+public class JsonArray extends ArrayList<Object> {
+}
---
+++ src/main/java/com/cmsen/common/lang/JsonObject.java
@@ -0,0 +1,13 @@
+/**
+ * +---------------------------------------------------------
+ * | Author Jared.Yan<yanhuaiwen@163.com>
+ * +---------------------------------------------------------
+ * | Copyright (c) http://cmsen.com All rights reserved.
+ * +---------------------------------------------------------
+ */
+package com.cmsen.common.lang;
+
+import java.util.LinkedHashMap;
+
+public class JsonObject extends LinkedHashMap<Object, Object> {
+}
