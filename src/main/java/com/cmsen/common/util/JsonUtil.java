a950623845fc26c96ee9fccd600c3b3695db9e50
Jared.Yan <fuile@qq.com>
2021-06-03 13:08:17 +0800

    update 2.2.0

--- src/main/java/com/cmsen/common/util/JsonUtil.java
+++ src/main/java/com/cmsen/common/util/JsonUtil.java
@@ -14,6 +14,7 @@ import java.util.Map;
  *
  * @author jared.Yan (yanhuaiwen@163.com)
  */
+@Deprecated
 public class JsonUtil {
     public static Map<?, ?> toObject(String string) {
         return toClass(string, Map.class);
