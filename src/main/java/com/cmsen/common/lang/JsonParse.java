09e56dcaaecf4f25e0ad4799a35ff62b5e7e97e7
Jared.Yan <fuile@qq.com>
2021-06-11 12:01:50 +0800

    update 2.2.0

---
+++ src/main/java/com/cmsen/common/lang/JsonParse.java
@@ -0,0 +1,27 @@
+/**
+ * +---------------------------------------------------------
+ * | Author Jared.Yan<yanhuaiwen@163.com>
+ * +---------------------------------------------------------
+ * | Copyright (c) http://cmsen.com All rights reserved.
+ * +---------------------------------------------------------
+ */
+package com.cmsen.common.lang;
+
+import com.cmsen.common.util.JSON;
+
+public abstract class JsonParse<T> {
+    protected T data;
+
+    protected JsonParse(String value) {
+        this.data = JSON.parse(value);
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    @Override
+    public String toString() {
+        return data != null ? data.toString() : null;
+    }
+}
--- src/test/java/com/cmsen/common/http/ClientHttpTest.java
+++ src/test/java/com/cmsen/common/http/ClientHttpTest.java
@@ -14,8 +14,10 @@ public class ClientHttpTest extends TestCase {
         // httpRequest.setHeaders("Content-Type", "multipart/form-data");
         File file = new File("doc/image.png");
         File file2 = new File("doc/img_1.png");
+        // {"id":"2","name":"cli"}
 
         httpRequest.setStreamBinary("name", "asdsadasdsad", "text/plain");
+        httpRequest.setStreamBinary("json", "{\"id\":\"2\",\"name\":\"cli\"}", "application/json");
         httpRequest.setStreamBinary("file", "image1.jpg", file);
         httpRequest.setStreamBinary("file", "image2.jpg", file2);
 
