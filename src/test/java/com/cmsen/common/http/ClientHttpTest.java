b37f8f2899e23c5f0006a59a21573ed1e2a0165f
Jared.Yan <fuile@qq.com>
2021-06-11 11:28:26 +0800

    update 2.2.0

---
+++ src/test/java/com/cmsen/common/http/ClientHttpTest.java
@@ -0,0 +1,32 @@
+package com.cmsen.common.http;
+
+import com.cmsen.common.util.StringUtil;
+import junit.framework.TestCase;
+
+import java.io.File;
+
+public class ClientHttpTest extends TestCase {
+    public void test() {
+
+        ClientHttpRequest httpRequest = new ClientHttpRequest();
+        httpRequest.setUrl("http://localhost:14556/api/upload/name");
+
+        // httpRequest.setHeaders("Content-Type", "multipart/form-data");
+        File file = new File("doc/image.png");
+        File file2 = new File("doc/img_1.png");
+
+        httpRequest.setStreamBinary("name", "asdsadasdsad", "text/plain");
+        httpRequest.setStreamBinary("file", "image1.jpg", file);
+        httpRequest.setStreamBinary("file", "image2.jpg", file2);
+
+
+        ClientHttpResponse response = ClientHttp.post(httpRequest);
+
+
+        System.err.println(httpRequest.getHeaders());
+        System.err.println(httpRequest.getStreamBinary());
+        System.err.println(response);
+        System.err.println(StringUtil.randomString(16));
+
+    }
+}
